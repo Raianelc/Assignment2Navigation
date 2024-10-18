226382b2f4b7f894afe8b5333d3e03ac47d0c078
Raianelc <71954@student.dorset-college.ie>
2024-10-18 21:14:26 +0100

    Final Changes

--- app/src/main/java/com/stu71954/assignment2navigation/MainActivity.kt
+++ app/src/main/java/com/stu71954/assignment2navigation/MainActivity.kt
@@ -13,14 +13,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.stu71954.assignment2navigation.ui.theme.Assignment2NavigationTheme
 
+
+//Here I'm calling the Navigation composable function in the setContent block to set up the navigation graph for the app.
+//The first screen still being the LandingPage composable function, as I set up as the start destination.
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Navigation()
-            //Here I'm calling the Navigation composable function in the setContent block to set up the navigation graph for the app.
-            //The first screen still being the LandingPage composable function, as I set up as the start destination.
         }
     }
 }
